b5c1af4b17baef537883f0d1c70888db87edb292
ALEX <Alex.XiaT@gmail.com>
2013-05-26 22:18:41 -0400

    3sum_closest

---
+++ 3sum_closest.java
@@ -0,0 +1,69 @@
+public class Solution {
+    public int threeSumClosest(int[] num, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        //First sort the given list
+		int c = 0;
+		int max = 0;
+		for(int a : num){
+			if(c == 0){
+				max = a;
+				c++;
+			} else {
+				if(max < a)
+					max = a;
+			}
+		}
+		//Now res is the sorted array
+		int[] res = sorting(num, max);
+		int minSub = Integer.MAX_VALUE;
+		int min = 0;
+		for(int i = 0; i < res.length - 2; i++){
+			int j = i+1;
+			int k = res.length - 1;
+			
+			while(j < k){
+				int sub = Math.abs(target - res[i] - res[j] - res[k]);
+				if(sub == 0)
+					return 0;
+				if(sub < minSub){
+					minSub = sub;
+					min = res[i] + res[j] + res[k];
+				} 
+				if(res[i]+res[j]+res[k] > target)
+					k--;
+				else 
+					j++;
+			}
+		}
+		
+		
+		
+		
+		
+		
+    }
+	
+	public static int[] sorting(int[] num, int range){
+		int[] a = new int[range+1];
+		//System.out.println("size => "+range);
+		for(int tmp : num){
+			int count = a[tmp];
+			count++;
+			a[tmp] = count;
+		}
+		int size = num.length;
+		int[] res  = new int[size];
+		int pcount = 0;
+		for(int i = 0; i < a.length; i++){
+			int count = a[i];
+			while(count > 0){
+				res[pcount] = i;
+				count--;
+				pcount++;
+			}
+		}
+		return res;
+	}
+	
+}
